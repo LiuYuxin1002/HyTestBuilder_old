9489478de43422e58cf750577ac58561795bc879
liuyuxinzju <21725152@zju.edu.cn>
2019-05-15 21:53:19 +0800

    为项目添加Redis； 写了一些redis中间件； 下一件事就是写从Redis真正读数据的步骤，关键！！

---
+++ EtherCATImpl/database/RedisHashHelper.cs
@@ -0,0 +1,101 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtherCATImpl.database
+{
+    public class RedisHashHelper
+    {
+        private const int KEY = 1;
+        private const int VALUE = 2;
+        static RedisHashHelper() { }
+
+        /// <summary>
+        /// 为key对应的hash增加一个键值对
+        /// </summary>
+        public static void AddKeyValue(RedisClient client, string key, string field, string value)
+        {
+            long output = client.HSet(key, stringToBytes(field), stringToBytes(value));
+        }
+        /// <summary>
+        /// 批量增加键值对
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="key"></param>
+        /// <param name="dicToAdd"></param>
+        public static void AddKeyValues(RedisClient client, string key, Dictionary<string, string> dicToAdd)
+        {
+            client.HMSet(key, stringToBytesArr(dicToAdd, KEY), stringToBytesArr(dicToAdd, VALUE));
+        }
+        /// <summary>
+        /// 获取单个键值对
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="key"></param>
+        /// <param name="field"></param>
+        /// <returns></returns>
+        public static string GetKeyValue(RedisClient client, string key, string field)
+        {
+            byte[] strbytes = client.HGet(stringToBytes(key), stringToBytes(field));
+            return bytesToString(strbytes);
+        }
+        /// <summary>
+        /// 获取key对应的全部键值对
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static Dictionary<string, string> GetKeyValues(RedisClient client, string key)
+        {
+            byte[][] fields = client.HKeys(key);//可能比较慢，算法要改进
+            byte[][] values = client.HMGet(key, fields);
+            return BytesArrToDic(fields, values);
+        }
+        /// <summary>
+        /// 删除单个
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="key"></param>
+        /// <param name="field"></param>
+        public static void DeleteKeyValues(RedisClient client, string key, string field)
+        {
+            long output = client.HDel(key, stringToBytes(field));
+        }
+        /// <summary>
+        /// 删除key对应的全部hash
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="key"></param>
+        public static void DeleteAllHash(RedisClient client, string key)
+        {
+            client.Del(key);
+        }
+
+        #region utils
+        
+        private static byte[] stringToBytes(string str)
+        {
+            return Encoding.UTF8.GetBytes(str);
+        }
+
+        //未实现
+        private static byte[][] stringToBytesArr(Dictionary<string, string> dic, int keyorvalue)
+        {
+            return null;
+        }
+
+        private static string bytesToString(byte[] bytes)
+        {
+            return Encoding.UTF8.GetString(bytes);
+        }
+        //未实现
+        private static Dictionary<string, string> BytesArrToDic(byte[][] keys, byte[][] values)
+        {
+            return new Dictionary<string, string>();
+        }
+        #endregion
+    }
+}
--- MasterConnectorConsole/Program.cs
+++ MasterConnectorConsole/Program.cs
@@ -4,88 +4,27 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using ServiceStack.Redis;
 
 namespace MasterConnectorConsole
 {
-    //单个从站
-    [StructLayout(LayoutKind.Sequential)]
-    public struct SlaveInfo
-    {
-        public int state;
-        public string name;
-    }
-
-    //试验环境上下文信息
-    [StructLayout(LayoutKind.Sequential)]
-    public struct Context
-    {
-        public List<StringBuilder> nicDesc;
-        public List<StringBuilder> nicName;
-        public int adapterNum;
-        public int slaveNum;
-    }
-
-    public class InterfaceClass
-    {
-        //获取计算机网卡信息，接收到Context结构体
-        [DllImport("DLLOutput.dll")]
-        public static extern int getAdapterNum();
-        [DllImport("DLLOutput.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
-        public static extern int getContextInfo(StringBuilder str);
-        //设置所选网卡，如果失败返回错误信息，成功返回当前连接的从站信息
-        [DllImport("DLLOutput.dll", EntryPoint = "setNicId", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int setNicId(int nicId);
-        //自动配置从站，成功更新结构体数组
-        [DllImport("DLLOutput.dll")]
-        public static extern int initConfig();
-        [DllImport("DLLOutput.dll", EntryPoint = "setNicInfo", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void getSlaveInfo(ref SlaveInfo slaveInfo, int id);
-        //设置从站某端口信息
-        [DllImport("DLLOutput.dll")]
-        public static extern int setIntergerValue(int slaveId, int channel, int value);
-        [DllImport("DLLOutput.dll")]
-        public static extern int setDoubleValue(int slaveId, int channel, double value);
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Context context = new Context();
-            context.nicName = new List<StringBuilder>(10);
+            RedisClient client = new RedisClient("127.0.0.1", 6379);
 
-            int adapterNum = InterfaceClass.getAdapterNum();
-            Console.WriteLine("您的网卡信息如下, 请选择：");
-            for (int i=0; i< adapterNum; i++)
-            {
-                StringBuilder strb = new StringBuilder(128);
-                int nicNum = InterfaceClass.getContextInfo(strb);
-                context.nicName.Add(strb);
-                Console.WriteLine("[" + i + "] :" + strb);
-            }
-            //读取选择网卡
-            int selected = int.Parse(Console.ReadLine());
-            int result = InterfaceClass.setNicId(selected);
+            client.Add("123", "kkk");
+            Console.WriteLine(client.Get<string>("123"));
 
-            if (result <= 0)//连接失败
+            List<string> keyStrList = client.GetAllKeys();
+            keyStrList.ForEach(delegate (string key)
             {
-                Console.WriteLine("当前选择网卡没有连接从站，请检查");
-                return;
-            }
-            
-            //如果连接成功，建立从站结构体数组
-            SlaveInfo[] slaveinfo = new SlaveInfo[context.slaveNum];
-            //显示从站信息
-            Console.WriteLine("您的计算机连接的从站信息如下：");
-            for (int i=0; i<context.slaveNum; i++)
-            {
-                InterfaceClass.getSlaveInfo(ref slaveinfo[i], i);
-                Console.WriteLine(i+":"+slaveinfo[i].name);
-            }
+                Console.WriteLine(key + "=>" + client.Get<string>(key));
+            });
 
-            //写入测试
-            InterfaceClass.setIntergerValue(1, 1, 240);
-            InterfaceClass.getSlaveInfo(ref slaveinfo[1], 1);
-            Console.WriteLine("写入测试结果:" + slaveinfo[1].state);
+            client.HSet("1", Encoding.UTF8.GetBytes("123"), Encoding.UTF8.GetBytes("234"));
+            client.HSet("1", Encoding.UTF8.GetBytes("111"), Encoding.UTF8.GetBytes("2jqk"));
         }
     }
 }
