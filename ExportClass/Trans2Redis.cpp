e5b28cb00fd435db5ba4a7994e0e54af032da8a9
Irvin0412 <OuYangYang1997@outlook.com>
2019-05-20 12:03:56 +0800

    加上了Redis的C++配置 加了写好的Redis C++接口

---
+++ ExportClass/Trans2Redis.cpp
@@ -0,0 +1,149 @@
+#include"Trans2Redis.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+	void Trans2Redis::slave2Hash() 
+	{
+		for (int i = 0; i < ec_slavecount; i++)
+		{
+			//判断
+			
+			#pragma region
+			//判断从站类型
+			switch (my_slave[i].type)
+			{
+			case 1://DI
+			{
+					   //判断端口数量
+					   switch (my_slave[i].channelNum)
+					   {
+					   case 2:
+					   {
+								 (SoemImplClass::slave_di2)my_slave[i].ptrToSlave;
+								 *((SoemImplClass::slave_di2)my_slave[i].ptrToSlave)->values;
+								 for (int j = 0; j<my_slave[i].channelNum; j++)
+								 {
+									 string KEY = to_string(i)+"_"+to_string(j);//slave id_channal id									 
+									 int VALUE_temp = (int)(*((SoemImplClass::slave_di2)my_slave[i].ptrToSlave)->values + j);
+
+									 string VALUE = to_string(VALUE_temp);
+									 
+									 TIME.insert(pair<string, string>(KEY, VALUE));
+								 }
+								 break;
+					   }
+
+					   case 4:
+					   {
+								 (SoemImplClass::slave_di4)my_slave[i].ptrToSlave;
+								 *((SoemImplClass::slave_di4)my_slave[i].ptrToSlave)->values;
+								 for (int j = 0; j<my_slave[i].channelNum; j++)
+								 {
+									 string KEY = to_string(i) + "_" + to_string(j);//slave id_channal id									 
+									 int VALUE_temp = (int)(*((SoemImplClass::slave_di4)my_slave[i].ptrToSlave)->values + j);
+
+									 string VALUE = to_string(VALUE_temp);
+
+									 TIME.insert(pair<string, string>(KEY, VALUE));
+								 }
+								 break;
+					   }
+					   case 8: {
+								   (SoemImplClass::slave_di8)my_slave[i].ptrToSlave;
+								   *((SoemImplClass::slave_di8)my_slave[i].ptrToSlave)->values;
+								   for (int j = 0; j<my_slave[i].channelNum; j++)
+								   {
+									   string KEY = to_string(i) + "_" + to_string(j);//slave id_channal id									 
+									   int VALUE_temp = (int)(*((SoemImplClass::slave_di8)my_slave[i].ptrToSlave)->values + j);
+
+									   string VALUE = to_string(VALUE_temp);
+
+									   TIME.insert(pair<string, string>(KEY, VALUE));
+								   }
+								   break;
+					   }
+					   default:
+						   break;
+					   }
+			}
+			case 2://DO
+			{
+					   switch (my_slave[i].channelNum)
+					   {
+					   case 2:
+
+					   case 4:
+
+					   case 8:
+
+					   default:
+						   break;
+					   }
+					   break;
+			}
+			case 3://AI
+			{
+					   switch (my_slave[i].channelNum)
+					   {
+					   case 1:
+
+					   case 2:
+
+					   case 4:
+
+					   case 8:
+
+					   default:
+						   break;
+					   }
+					   break;
+			}
+			case 4://AO
+			{
+					   switch (my_slave[i].channelNum)
+					   {
+					   case 1:
+
+					   case 2:
+
+					   case 4:
+
+					   case 8:
+
+					   default:
+						   break;
+					   }
+					   break;
+			}
+			default:
+				break;
+			}
+			#pragma endregion
+
+		}
+	}
+
+	void Trans2Redis::hash2Redis(hash_map<string, string> hashmap)
+	{
+		redisContext*c = redisConnect("127.0.0.1", 6379);
+		if (c->err)
+		{
+			redisFree(c);
+			return ;
+		}
+
+		for (hash_map<string, string>::iterator i = hashmap.begin(); i != hashmap.end(); i++)
+		{
+			redisReply* r = (redisReply*)redisCommand(c, "HSET TransHash %s %s", i->first, i->second);
+			freeReplyObject(r);
+		}
+	}
+
+#ifdef __cplusplus
+}
+#endif
